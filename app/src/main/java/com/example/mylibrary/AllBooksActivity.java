c558178b418e617dcd6d297ced9fa7695a097db3
riyaminiarora <riaarora134@gmail.com>
2020-07-20 22:46:50 +0530

    initial commit

---
+++ app/src/main/java/com/example/mylibrary/AllBooksActivity.java
@@ -0,0 +1,18 @@
+package com.example.mylibrary;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class AllBooksActivity extends AppCompatActivity {
+    private RecyclerView booksRecView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_books);
+        booksRecView=findViewById(R.id.booksRecView);
+
+    }
+}
---
+++ app/src/main/java/com/example/mylibrary/BookRecViewAdapter.java
@@ -0,0 +1,57 @@
+package com.example.mylibrary;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class BookRecViewAdapter extends  RecyclerView.Adapter<BookRecViewAdapter.ViewHolder>{
+    private ArrayList<Book> books=new ArrayList<>();
+    private Context mContext;
+
+    public BookRecViewAdapter(Context mContext) {
+        this.mContext = mContext;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_book,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        
+    }
+
+    @Override
+    public int getItemCount() {
+        return books.size();
+    }
+
+    public void setBooks(ArrayList<Book> books) {
+        this.books = books;
+        notifyDataSetChanged();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        private CardView parent;
+        private ImageView imgBook;
+        private TextView txtName;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            parent=itemView.findViewById(R.id.parent);
+            imgBook=itemView.findViewById(R.id.imgBook);
+            txtName=itemView.findViewById(R.id.txtName);
+        }
+    }
+}
